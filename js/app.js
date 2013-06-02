ae039a0e714bbd9d0a98520949cf630e40bab6ba
John Kurkowski <john.kurkowski@gmail.com>
2013-06-02 01:44:50 -0700

    Begin homepage splash
    * Header splash resizes
    * Nav scrolls with you until ToC
    * Introduce Angular; give into jQuery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,62 @@
+angular
+  .module('johnkurkowski', [])
+  .directive('backgroundResizeHeightRatio', function() {
+    return function(scope, element, attrs) {
+      var bg = /url\(([^)]+)/.exec(element.css('background-image'))[1],
+          resize = function() {
+            var ratio = parseFloat(attrs.backgroundResizeHeightRatio);
+            element.height(element.width() / ratio);
+            scope.$digest();
+          };
+
+      angular.element('<img>').load(function() {
+        resize();
+        angular.element(window).on('resize', resize);
+      }).attr('src', bg);
+    };
+  })
+  .directive('scrollBottomWithin', function() {
+    return function(scope, element, attrs) {
+      var selector = attrs.scrollBottomWithin,
+          withinEl = angular.element(selector),
+
+          scrollBottom = function() {
+            var win = angular.element(window),
+                viewportHeight = win.scrollTop() + win.innerHeight(),
+                includeMargin = true,
+                withinTop = withinEl.offset().top + withinEl.height();
+
+            if (viewportHeight < withinTop) {
+              element.css({
+                position: 'fixed',
+                top: 'auto',
+                bottom: 0
+              });
+            }
+            else {
+              element.css({
+                position: 'absolute',
+                top: withinTop - element.outerHeight(includeMargin),
+                bottom: 'auto'
+              });
+            }
+          },
+
+          watchDims = function() {
+            var els = element.add(withinEl);
+                dims = els.map(function() {
+                  return { width: angular.element(this).width(), height: angular.element(this).height() };
+                }).toArray();
+            return dims;
+          },
+
+          deepEquals = true;
+
+      scope.$watch(watchDims, scrollBottom, deepEquals);
+      angular.element(document).on('scroll', scrollBottom);
+    };
+  });
+
 (function() {
   var disqus_loaded = false,
       load_disqus = function() {
