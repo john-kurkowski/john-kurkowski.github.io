8497be5dd85c1801f54e4f5c60b5321f7aaa03da
John Kurkowski <john.kurkowski@gmail.com>
2020-06-14 14:48:29 -0700

    Revert "Undo dark theme & non-standard variable names"
    This reverts commit 3d9b734911b8e5def0754dd433654b534d96be6e.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        navy: {
+          default: '#00173B'
+        },
+        redactive: {
+          default: '#FF3701'
+        }
+      },
+
       fontFamily: {
-        sans: ['Varela Round', ...defaultTheme.fontFamily.sans],
-        serif: ['Lora', ...defaultTheme.fontFamily.serif]
+        body: ['Lora'],
+        title: ['Varela Round']
       }
     }
   },
