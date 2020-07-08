09787915c41378f8632c0834c39d03d2adcddf93
John Kurkowski <john.kurkowski@gmail.com>
2020-07-08 14:41:04 -0700

    Switch to orange and blue colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        background: '#FBF9EB',
+        primary: '#E0992C',
+        secondary: '#1E4A79'
+      },
       fontFamily: {
         sans: ['Varela Round', ...defaultTheme.fontFamily.sans],
         serif: ['Lora', ...defaultTheme.fontFamily.serif]
