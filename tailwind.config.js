04d681d06707493ed3cb4dfc41d32afdcf1654c4
John Kurkowski <john.kurkowski@gmail.com>
2020-10-11 13:50:57 -0700

    Fix missing styles in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['src/**/*.{js,md,mdx}'],
+  purge: ['src/**/*.{js,md,mdx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
