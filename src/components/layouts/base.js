7531b838baf0d798d771fbf75d8424b2ac3e32f7
John Kurkowski <john.kurkowski@gmail.com>
2020-10-11 09:48:39 -0700

    Speed up Google Fonts loading
    Reduces the _appearance_ of fonts loading, jerking the page around.

--- src/components/layouts/base.js
+++ src/components/layouts/base.js
@@ -59,11 +59,11 @@ function Layout ({ children, page = {} }) {
         <link rel='preconnect' href='https://fonts.gstatic.com/' crossOrigin />
         <link
           as='style'
-          href='https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:wght@400;600;700;800&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:wght@400;600;700;800'
           rel='preload'
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:wght@400;600;700;800&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:wght@400;600;700;800'
           rel='stylesheet'
         />
 
