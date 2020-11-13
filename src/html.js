1f6066f528322ccfd323b79ef067e273e1329863
John Kurkowski <john.kurkowski@gmail.com>
2020-11-13 15:39:06 -0800

    Fix lint error
    * Format

--- src/html.js
+++ src/html.js
@@ -1,15 +1,15 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function HTML(props) {
+export default function HTML (props) {
   return (
-    <html {...props.htmlAttributes}>
+    <html lang='en-US' {...props.htmlAttributes}>
       <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta charSet='utf-8' />
+        <meta httpEquiv='x-ua-compatible' content='ie=edge' />
         <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no'
         />
         {props.headComponents}
       </head>
@@ -17,7 +17,7 @@ export default function HTML(props) {
         {props.preBodyComponents}
         <div
           key={`body`}
-          id="___gatsby"
+          id='___gatsby'
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
@@ -32,5 +32,5 @@ HTML.propTypes = {
   bodyAttributes: PropTypes.object,
   preBodyComponents: PropTypes.array,
   body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
+  postBodyComponents: PropTypes.array
 }
