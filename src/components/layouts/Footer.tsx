7f94caee3111bac59563ea86bd5e53f03f002e67
John Kurkowski <john.kurkowski@gmail.com>
2024-03-20 14:42:47 -0700

    Update social links
    * Remove Twitter. I don't use it anymore.
    * Add Instagram. I don't use it professionally, but it is a way to reach me.
    * Add Spotify. It is more professional than SoundCloud. Although I don't want to list every music service, as music is a hobby, not the main professional thrust of this website.

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import GitHubLogo from 'src/images/icons/iconmonstr-github-1.svg'
 import LinkedInLogo from 'src/images/icons/iconmonstr-linkedin-3.svg'
 import SoundcloudLogo from 'src/images/icons/iconmonstr-soundcloud-1.svg'
-import TwitterLogo from 'src/images/icons/iconmonstr-twitter-1.svg'
+import InstagramLogo from 'src/images/icons/iconmonstr-instagram-11.svg'
+import SpotifyLogo from 'src/images/icons/iconmonstr-spotify-1.svg'
 
 export default function Footer (): React.ReactElement {
   return (
@@ -11,16 +12,8 @@ export default function Footer (): React.ReactElement {
       <div className=''>
         <ul className='flex justify-center'>
           <li>
-            <a className='block icon-link' href='https://twitter.com/bluu'>
-              <figure>
-                <TwitterLogo />
-                <figcaption className='sr-only'>Twitter</figcaption>
-              </figure>
-            </a>
-          </li>
-          <li>
             <a
-              className='block icon-link pl-8'
+              className='block icon-link'
               href='https://www.linkedin.com/in/johnkurkowski'
             >
               <figure>
@@ -43,11 +36,33 @@ export default function Footer (): React.ReactElement {
           <li>
             <a
               className='block icon-link pl-8'
+              href='https://open.spotify.com/artist/741OEc1uZH9VMiQomSfoHb?si=MnN5PqGvSUqAGGfVntle9w'
+            >
+              <figure>
+                <SpotifyLogo />
+                <figcaption className='sr-only'>Spotify</figcaption>
+              </figure>
+            </a>
+          </li>
+          <li>
+            <a
+              className='block icon-link pl-8'
               href='https://soundcloud.com/bluu/'
             >
               <figure>
                 <SoundcloudLogo />
-                <figcaption className='sr-only'>Soundcloud</figcaption>
+                <figcaption className='sr-only'>SoundCloud</figcaption>
+              </figure>
+            </a>
+          </li>
+          <li>
+            <a
+              className='block icon-link pl-8'
+              href='https://instagram.com/johnkurkowski'
+            >
+              <figure>
+                <InstagramLogo />
+                <figcaption className='sr-only'>Instagram</figcaption>
               </figure>
             </a>
           </li>
