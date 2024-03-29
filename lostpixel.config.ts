85e71cb2a1c9ac21c0b57066c97c6ea0fc4b9268
John Kurkowski <john.kurkowski@gmail.com>
2024-03-29 14:24:04 -0700

    Copy Lost Pixel getting started instructions

---
+++ lostpixel.config.ts
@@ -0,0 +1,10 @@
+import { CustomProjectConfig } from 'lost-pixel'
+
+export const config: CustomProjectConfig = {
+  pageShots: {
+    pages: [{ path: '/', name: 'landing' }],
+    baseUrl: 'http://172.17.0.1:3000'
+  },
+  lostPixelProjectId: 'clud602ae10romo0e861bvpv2',
+  apiKey: process.env.LOST_PIXEL_API_KEY
+}
