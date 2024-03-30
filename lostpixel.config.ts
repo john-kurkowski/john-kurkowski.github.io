c032cb5ca7d82b4eb5641dcec2b82da5f0a3e147
John Kurkowski <john.kurkowski@gmail.com>
2024-03-29 20:27:28 -0700

    Visually test Gatsby production build, instead of development server
    * Copy npm scripts from newer upstream Gatsby starter

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -3,7 +3,7 @@ import { CustomProjectConfig } from 'lost-pixel'
 export const config: CustomProjectConfig = {
   pageShots: {
     pages: [{ path: '/', name: 'landing' }],
-    baseUrl: 'http://172.17.0.1:8000'
+    baseUrl: 'http://172.17.0.1:9000'
   },
   lostPixelProjectId: 'clud602ae10romo0e861bvpv2',
   apiKey: process.env.LOST_PIXEL_API_KEY
