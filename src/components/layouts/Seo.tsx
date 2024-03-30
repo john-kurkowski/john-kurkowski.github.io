3ac4105aebb1dcf6f5ca0c01ad461eff134c348b
John Kurkowski <john.kurkowski@gmail.com>
2024-03-30 00:49:30 -0700

    Move static head data to SSR
    Fixes FOUC while navigating. Font tags were swapping in and out.
    
    * Type check top level files too, like the new one in this commit

--- src/components/layouts/Seo.tsx
+++ src/components/layouts/Seo.tsx
@@ -20,14 +20,11 @@ export default function Seo(props: { page: Page }): React.ReactElement {
 
   return (
     <>
-      <html className='bg-secondary' lang='en-US' />
       <title>{title}</title>
       <meta property='og:title' content={title} />
 
       <meta name='author' content={props.page.site.siteMetadata.title} />
       <meta property='og:author' content={props.page.site.siteMetadata.url} />
-      <meta property='profile:first_name' content='John' />
-      <meta property='profile:last_name' content='Kurkowski' />
 
       <meta name='description' content={description} />
       <meta name='twitter:description' content={description} />
@@ -35,34 +32,6 @@ export default function Seo(props: { page: Page }): React.ReactElement {
       {props.page.dateForMeta && (
         <meta httpEquiv='date' content={props.page.dateForMeta} />
       )}
-
-      <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1.0, viewport-fit=cover'
-      />
-
-      <link
-        rel='preconnect'
-        href='https://fonts.gstatic.com/'
-        crossOrigin='anonymous'
-      />
-      <link
-        as='style'
-        href='https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:wght@400;600;700;800'
-        rel='preload'
-      />
-      <link
-        href='https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&family=Open+Sans:wght@400;600;700;800'
-        rel='stylesheet'
-      />
-
-      <link rel='shortcut icon' href='/favicon.ico' />
-      <link rel='apple-touch-icon' href='/avatar@2X.jpeg' />
-      <meta property='og:image' content='/avatar@2X.jpeg' />
-      <meta name='twitter:image:src' content='/avatar@2X.jpeg' />
-      <meta name='twitter:card' content='summary' />
-      <meta name='twitter:site' content='@bluu' />
-      <meta name='twitter:creator' content='@bluu' />
     </>
   )
 }
