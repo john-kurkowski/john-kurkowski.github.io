2b76677a85d4f6f820ff8a4e9107b40ffa51c957
John Kurkowski <john.kurkowski@gmail.com>
2024-04-03 15:47:50 -0700

    Visual test all routes
    * Upgrade ECMAScript version, for newer string matching functions

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,11 +1,31 @@
 import { CustomProjectConfig } from 'lost-pixel'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+function pagePaths(): string[] {
+  const siteUrl = 'https://johnkurkowski.com'
+  const sitemapXml = readFileSync(
+    join(__dirname, 'public', 'sitemap-0.xml'),
+    'utf-8',
+  )
+  const urlsRe = new RegExp(`${siteUrl}(/[^<]*)`, 'g')
+  return Array.from(sitemapXml.matchAll(urlsRe)).map(function ([, url]) {
+    return url.replaceAll('&amp;', '&')
+  })
+}
+
+function slugify(str: string): string {
+  const replaced = str.replaceAll('/', '-')
+  const stripped = replaced.replaceAll(/(^-+|-+$)/g, '')
+  return stripped ? stripped : 'index'
+}
 
 export const config: CustomProjectConfig = {
   apiKey: process.env.LOST_PIXEL_API_KEY,
   breakpoints: [414, 1280],
   lostPixelProjectId: 'clud602ae10romo0e861bvpv2',
   pageShots: {
-    pages: [{ path: '/', name: 'landing' }],
+    pages: pagePaths().map((path) => ({ path, name: slugify(path) })),
     baseUrl: 'http://172.17.0.1:9000',
   },
 }
