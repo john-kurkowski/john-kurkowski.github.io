996058f2e6e257d2b36398cb4890b95db454c785
John Kurkowski <john.kurkowski@gmail.com>
2024-04-03 15:09:06 -0700

    fixup! Merge remote-tracking branch 'origin/master' into lost-pixel

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -3,8 +3,8 @@ import { CustomProjectConfig } from 'lost-pixel'
 export const config: CustomProjectConfig = {
   pageShots: {
     pages: [{ path: '/', name: 'landing' }],
-    baseUrl: 'http://172.17.0.1:9000'
+    baseUrl: 'http://172.17.0.1:9000',
   },
   lostPixelProjectId: 'clud602ae10romo0e861bvpv2',
-  apiKey: process.env.LOST_PIXEL_API_KEY
+  apiKey: process.env.LOST_PIXEL_API_KEY,
 }
