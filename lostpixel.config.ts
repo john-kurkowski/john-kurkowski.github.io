acda705bf1cbb0226356091f15f1638bfe88e1fe
John Kurkowski <john.kurkowski@gmail.com>
2024-04-03 15:41:17 -0700

    Add breakpoints

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,10 +1,11 @@
 import { CustomProjectConfig } from 'lost-pixel'
 
 export const config: CustomProjectConfig = {
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+  breakpoints: [414, 1280],
+  lostPixelProjectId: 'clud602ae10romo0e861bvpv2',
   pageShots: {
     pages: [{ path: '/', name: 'landing' }],
     baseUrl: 'http://172.17.0.1:9000',
   },
-  lostPixelProjectId: 'clud602ae10romo0e861bvpv2',
-  apiKey: process.env.LOST_PIXEL_API_KEY,
 }
