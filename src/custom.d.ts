3ec60c831c14e59c56ad6eb72740efb6d478e589
John Kurkowski <john.kurkowski@gmail.com>
2026-03-29 22:35:55 -0700

    Typecheck Astro files

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,3 +2,9 @@ declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
   export default content
 }
+
+interface Window {
+  DISQUS?: {
+    reset(options: { reload: boolean }): void
+  }
+}
